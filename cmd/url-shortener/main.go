dbc45da32e8fe06776a650bd89d2c0d005af40da
Daniil Kozlovsky <danil.kozlovskiw@gmail.com>
2025-08-11 13:07:13 +0300

    Add Go module dependencies (go.mod and go.sum)

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,13 @@
+package main 
+
+func main() {
+	// TODO: init config: cleanenv
+
+	// TODO: init logger: slog
+
+	// TODO: init storage: sqlite
+
+	// TODO: init router: chi, render
+
+	// TODO: run server:   
+}
