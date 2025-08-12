afda2a4402ea00ff194a69bc1f7b65f2f5fd8a6e
Daniil Kozlovsky <danil.kozlovskiw@gmail.com>
2025-08-12 17:42:38 +0300

    bugs fixed

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,37 +4,38 @@ import (
 	"log"
 	"os"
 	"time"
+
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
-	Env         string `yaml:"env" env:"ENV" env-default:"local"`
+	Env         string `yaml:"env" env-default:"local"`
 	StoragePath string `yaml:"storage_path" env-required:"true"`
-	HTTPServer `yaml:"http_server"`
+	HTTPServer  `yaml:"http_server"`
 }
 
 type HTTPServer struct {
 	Address     string        `yaml:"address" env-default:"localhost:8080"`
-	TimeOut     time.Duration `yaml:"timeout" env-default:"4 s "`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
 }
 
- func MustLoad() *Config {
+func MustLoad() *Config {
 	configPath := os.Getenv("CONFIG_PATH")
 	if configPath == "" {
-		 log.Fatal("Config path is not set ")
+		log.Fatal("CONFIG_PATH is not set")
 	}
 
-	//check if file exist
+	// check if file exists
 	if _, err := os.Stat(configPath); os.IsNotExist(err) {
-		log.Fatalf("Config file is not exist: %s", configPath)
+		log.Fatalf("config file does not exist: %s", configPath)
 	}
 
-	var cfg Config 
+	var cfg Config
 
 	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
-		log.Fatalf("Cannot read config: %s", err)
+		log.Fatalf("cannot read config: %s", err)
 	}
 
 	return &cfg
- }
+}
