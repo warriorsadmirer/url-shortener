74c251e80cefed5056afeb1e1b19d7f9503bba72
Daniil Kozlovsky <danil.kozlovskiw@gmail.com>
2025-08-13 15:38:01 +0300

    hot fix

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,8 +1,15 @@
-package main 
+package main
+
+import (
+	"fmt"
+	"url-shortener/internal/config"
+)
 
 func main() {
-	// TODO: init config: cleanenv
 
+
+    cfg := config.MustLoad()
+	fmt.Println(cfg)
 	// TODO: init logger: slog
 
 	// TODO: init storage: sqlite
