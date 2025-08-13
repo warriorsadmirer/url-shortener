ca5fc34027f942ee82e798851522f8a186c4eea4
Daniil Kozlovsky <danil.kozlovskiw@gmail.com>
2025-08-13 16:12:41 +0300

    add structured logging with slog

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,20 +1,48 @@
 package main
 
 import (
-	"fmt"
+ 	"log/slog"
+	"os"
 	"url-shortener/internal/config"
 )
-
+const (
+	envLocal = "local"
+	envDev = "dev"
+	envProd = "prod "
+)
 func main() {
 
 
     cfg := config.MustLoad()
-	fmt.Println(cfg)
-	// TODO: init logger: slog
 
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url shortener", slog.String("env", cfg.Env ))
+	log.Debug("Debug messages are enabled ")
 	// TODO: init storage: sqlite
 
 	// TODO: init router: chi, render
 
 	// TODO: run server
 }
+
+func setupLogger(env string) *slog.Logger {
+
+	var log *slog.Logger 
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}), 
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo }),
+		)
+	}
+
+	return log
+} 
