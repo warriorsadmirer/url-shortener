fc5f56105863d5bfcca1eb5b901f6a97c9da0672
Daniil Kozlovsky <danil.kozlovskiw@gmail.com>
2025-08-19 18:09:47 +0300

    Checking the functionality of the SaveURL func

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -5,30 +5,45 @@ import (
 	"os"
 	"url-shortener/internal/config"
 	"url-shortener/internal/lib/logger/sl"
- 	"url-shortener/internal/storage/sqlite"
+	"url-shortener/internal/storage/sqlite"
 )
+
 const (
 	envLocal = "local"
-	envDev = "dev"
-	envProd = "prod "
+	envDev   = "dev"
+	envProd  = "prod "
 )
+
 func main() {
 
-
-    cfg := config.MustLoad()
+	cfg := config.MustLoad()
 
 	log := setupLogger(cfg.Env)
 
-	log.Info("starting url shortener", slog.String("env", cfg.Env ))
+	log.Info("starting url shortener", slog.String("env", cfg.Env))
 	log.Debug("Debug messages are enabled ")
-	
+
 	storage, err := sqlite.New(cfg.StoragePath)
 	if err != nil {
-		log.Error("failed to init storage ", sl.Err(err ))
+		log.Error("failed to init storage ", sl.Err(err))
 		os.Exit(1)
 	}
 
-	_ = storage 
+	id, err := storage.SaveURL("https://google.com", "google")
+	if err != nil {
+		log.Error("failed to save url ", sl.Err(err))
+		os.Exit(1)
+	}
+
+	log.Info("saved URL", slog.Int64("id", id))
+
+	id, err = storage.SaveURL("https://google.com", "google")
+	if err != nil {
+		log.Error("failed to save url", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
 
 	// TODO: init router: chi, render
 
@@ -37,11 +52,11 @@ func main() {
 
 func setupLogger(env string) *slog.Logger {
 
-	var log *slog.Logger 
+	var log *slog.Logger
 	switch env {
 	case envLocal:
 		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}), 
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
 		)
 	case envDev:
 		log = slog.New(
@@ -49,9 +64,9 @@ func setupLogger(env string) *slog.Logger {
 		)
 	case envProd:
 		log = slog.New(
-			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo }),
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
 		)
 	}
 
 	return log
-} 
+}
