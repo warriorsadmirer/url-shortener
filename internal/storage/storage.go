753256bfa9f2907479c9194b848ca4ccd22517ed
Daniil Kozlovsky <danil.kozlovskiw@gmail.com>
2025-08-19 18:05:07 +0300

    fix

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -3,6 +3,6 @@ package storage
 import "errors"
 
 var (
-	ErrURLNotFound = errors.New("URL not found")
-	ErrURLExists = errors.New("URL exists")
+	ErrURLNotFound = errors.New("url not found")
+	ErrURLExists   = errors.New("url exists")
 )
